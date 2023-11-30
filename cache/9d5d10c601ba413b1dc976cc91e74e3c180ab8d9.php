9be6ad8df233ca74c60d321a892bf85036c9dd0c
Vladimir <vrakita@yahoo.com>
2023-11-30 15:42:50 +0100

    Updated .gitignore

---
+++ cache/9d5d10c601ba413b1dc976cc91e74e3c180ab8d9.php
@@ -0,0 +1,124 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<title>Dating sugardaddy</title>
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+		<link rel="stylesheet" href="assets/lp1/css/style.css">
+		<meta name="description" content="">
+		<meta name="author" content="">
+	</head>
+
+	<body>
+
+		<main>
+			<section class="hero">
+				<div class="popup active" id="popup1">
+					<div class="popup__overlay"></div>
+					<div class="popup__content">
+						<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta velit suscipit illum, libero optio aspernatur. Eius dolorem dolores recusandae labore!</p>
+						<div class="loader-wrapper">
+					        <div class="circle circle-1"></div>
+					        <div class="circle circle-2"></div>
+					        <div class="circle circle-3"></div>
+					        <div class="circle circle-4"></div>
+					        <div class="circle circle-5"></div>
+					    </div>
+
+						<div class="confirm-box" style="display: none;">
+							<a href="javascript:;" class="btn confirm">Confirm</a>
+						    <p>WE FOUND <strong class="number"></strong> in <strong class="city"></strong></p>
+						</div>
+					</div>
+				</div>
+
+				<div class="popup " id="popup2">
+					<div class="popup__overlay"></div>
+					<div class="popup__content">
+						<p>Lorem ipsum dolor sit amet consectetur, adipisicing 18 elit. </p>
+						<div class="btn-wrapper">
+							<a href="javascript:;" class="btn next">Yes</a>
+							<a href="javascript:;" class="btn next">No</a>
+						</div>
+					</div>
+				</div>
+
+				<div class="popup " id="popup3">
+					<div class="popup__overlay"></div>
+					<div class="popup__content">
+						<p>Lorem ipsum dolor sit amet consectetur, adipisicing 18 Gender. </p>
+						<div class="btn-wrapper">
+							<a href="javascript:;" class="btn gender">Male</a>
+							<a href="javascript:;" class="btn gender">Female</a>
+						</div>
+					</div>
+				</div>
+
+				<div class="popup " id="popup4">
+					<div class="popup__overlay"></div>
+					<div class="popup__content">
+						<p>Lorem ipsum dolor sit amet consectetur, adipisicing relationship. </p>
+						<div class="btn-wrapper">
+							<a href="javascript:;" class="btn relationship">Yes</a>
+							<a href="javascript:;" class="btn relationship">No</a>
+						</div>
+					</div>
+				</div>
+
+				<div class="popup " id="popup5">
+					<div class="popup__overlay"></div>
+					<div class="popup__content">
+						<h1>Congratulations!</h1>
+						<p>Your perfect partner is waiting for you! Sign up and chat now!</p>
+						<a href="javascript:;" class="btn">Start</a>
+					</div>
+				</div>
+
+				<div class="map-pin map-pin-1">
+					<div class="img-wrapper" style="background-image: url('assets/lp1/images/u1.jpg')"></div>
+					<p>Tiffany <img src="assets/lp1/images/pin.svg" alt="" width="20"> 1250m</p>
+					<a href="javascript:;" class="btn">Click to view</a>
+				</div>
+
+				<div class="map-pin map-pin-2">
+					<div class="img-wrapper" style="background-image: url('assets/lp1/images/u2.jpg')"></div>
+					<p>Tiffany <img src="assets/lp1/images/pin.svg" alt="" width="20"> 1250m</p>
+					<a href="javascript:;" class="btn">Click to view</a>
+				</div>
+
+				<div class="map-pin map-pin-3">
+					<div class="img-wrapper" style="background-image: url('assets/lp1/images/u3.jpg')"></div>
+					<p>Tiffany <img src="assets/lp1/images/pin.svg" alt="" width="20"> 1250m</p>
+					<a href="javascript:;" class="btn">Click to view</a>
+				</div>
+
+				<div class="map-pin map-pin-4">
+					<div class="img-wrapper" style="background-image: url('assets/lp1/images/u4.jpg')"></div>
+					<p>Tiffany <img src="assets/lp1/images/pin.svg" alt="" width="20"> 1250m</p>
+					<a href="javascript:;" class="btn">Click to view</a>
+				</div>
+
+				<div class="map-pin map-pin-5">
+					<div class="img-wrapper" style="background-image: url('assets/lp1/images/u5.jpg')"></div>
+					<p>Tiffany <img src="assets/lp1/images/pin.svg" alt="" width="20"> 1250m</p>
+					<a href="javascript:;" class="btn">Click to view</a>
+				</div>
+				<div class="map-pin map-pin-6">
+					<div class="img-wrapper" style="background-image: url('assets/lp1/images/u6.jpg')"></div>
+					<p>Tiffany <img src="assets/lp1/images/pin.svg" alt="" width="20"> 1250m</p>
+					<a href="javascript:;" class="btn">Click to view</a>
+				</div>
+			</section>
+		</main>
+
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+		<script src="assets/lp1/js/app.js"></script>
+
+	</body>
+
+</html><?php /**PATH /Users/vrakita/projects/local/datingpages/views/templates/lp1.blade.php ENDPATH**/ ?>
