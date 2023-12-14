479891a01ac6721c986108dd6cfc7dec9bedbb60
Vladimir <vrakita@yahoo.com>
2023-12-14 09:05:08 +0100

    Added lp3 and all translations.

--- views/templates/lp2.blade.php
+++ views/templates/lp2.blade.php
@@ -13,14 +13,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
-    <meta http-equiv="delegate-ch" content="sec-ch-ua https://track.1234sd123.com; sec-ch-ua-mobile https://track.1234sd123.com; sec-ch-ua-arch https://track.1234sd123.com; sec-ch-ua-model https://track.1234sd123.com; sec-ch-ua-platform https://track.1234sd123.com; sec-ch-ua-platform-version https://track.1234sd123.com; sec-ch-ua-bitness https://track.1234sd123.com; sec-ch-ua-full-version-list https://track.1234sd123.com; sec-ch-ua-full-version https://track.1234sd123.com"><style>.dtpcnt{opacity: 0;}</style>
-    <script>
-        (function(b,c,l,q,r,e,t,y,d,m,u,z,a,n,g,f,p,v,h,w){function x(){for(var a=c.querySelectorAll(".dtpcnt"),b=0,d=a.length;b<d;b++)a[b][t]=a[b][t].replace(/(^|\s+)dtpcnt($|\s+)/g,"")}w="https:"===b.location.protocol?"secure; ":"";b[d]||(b[d]=function(){(b[d].q=b[d].q||[]).push(arguments)},g=c[q],c[q]=function(){g&&g.apply(this,arguments);if(b[d]&&!b[d].hasOwnProperty("params")&&/loaded|interactive|complete/.test(c.readyState))for(;a=c[r][m++];)/\/?click\/?($|(\/[0-9]+)?$)/.test(a.pathname)&&(a[e]="javascrip"+
-                b.postMessage.toString().slice(4,5)+":"+d+'.l="'+a[e]+'",void 0')},setTimeout(function(){(p=/[?&]cpid(=([^&#]*)|&|#|$)/.exec(b.location.href))&&p[2]&&(f=p[2],v=c.cookie.match(new RegExp("(^| )vl-"+f+"=([^;]+)")));var d=c.cookie.match(/(^| )vl-cep=([^;]+)/),k=location[e];if("savedCep"===z&&d&&(!f||"undefined"===typeof f)&&0>k.indexOf("cep=")){var g=-1<k.indexOf("?")?"&":"?";k+=g+d.pop()}a=c.createElement("script");n=c.scripts[0];a.defer=1;a.src=u+(-1===u.indexOf("?")?"?":"&")+"lpref="+l(c.referrer)+
-                "&lpurl="+l(k)+"&lpt="+l(c.title)+"&vtm="+(new Date).getTime()+(v?"&uw=no":"");a[y]=function(){for(m=0;a=c[r][m++];)/dtpCallback\.l/.test(a[e])&&(a[e]=decodeURIComponent(a[e]).match(/dtpCallback\.l="([^"]+)/)[1]);x()};n.parentNode.insertBefore(a,n);f&&(h=new Date,h.setTime(h.getTime()+864E5),c.cookie="vl-"+f+"=1; "+w+"samesite=Strict; expires="+h.toGMTString()+"; path=/")},0),setTimeout(x,7E3))})(window,document,encodeURIComponent,"onreadystatechange","links","href","className","onerror","dtpCallback",
-                0,"https://track.1234sd123.com/d/.js","savedCep");
-    </script>
-    <noscript><link href="https://track.1234sd123.com/d/.js?noscript=true&lpurl=" rel="stylesheet"/></noscript>
+    @include('partials.script')
 </head>
 
 <body>
--- views/templates/lp3.blade.php
+++ views/templates/lp3.blade.php
@@ -1,122 +1,56 @@
 <!DOCTYPE html>
 
 <html lang="en">
-
     <head>
-
         <title>Dating Lander 1</title>
-
-        <meta charset="UTF-8" />
-
+        <meta charset="UTF-8"/>
         <link rel="shortcut icon" href="{{asset('favicon.ico')}}" type="image/x-icon">
-
-        <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0" />
-
+        <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0"/>
         <link rel="stylesheet" href="{{asset('style.css')}}">
-
-        <meta http-equiv="delegate-ch" content="sec-ch-ua https://track.1234sd123.com; sec-ch-ua-mobile https://track.1234sd123.com; sec-ch-ua-arch https://track.1234sd123.com; sec-ch-ua-model https://track.1234sd123.com; sec-ch-ua-platform https://track.1234sd123.com; sec-ch-ua-platform-version https://track.1234sd123.com; sec-ch-ua-bitness https://track.1234sd123.com; sec-ch-ua-full-version-list https://track.1234sd123.com; sec-ch-ua-full-version https://track.1234sd123.com"><style>.dtpcnt{opacity: 0;}</style>
-		<script>
-			(function(b,c,l,q,r,e,t,y,d,m,u,z,a,n,g,f,p,v,h,w){function x(){for(var a=c.querySelectorAll(".dtpcnt"),b=0,d=a.length;b<d;b++)a[b][t]=a[b][t].replace(/(^|\s+)dtpcnt($|\s+)/g,"")}w="https:"===b.location.protocol?"secure; ":"";b[d]||(b[d]=function(){(b[d].q=b[d].q||[]).push(arguments)},g=c[q],c[q]=function(){g&&g.apply(this,arguments);if(b[d]&&!b[d].hasOwnProperty("params")&&/loaded|interactive|complete/.test(c.readyState))for(;a=c[r][m++];)/\/?click\/?($|(\/[0-9]+)?$)/.test(a.pathname)&&(a[e]="javascrip"+
-					b.postMessage.toString().slice(4,5)+":"+d+'.l="'+a[e]+'",void 0')},setTimeout(function(){(p=/[?&]cpid(=([^&#]*)|&|#|$)/.exec(b.location.href))&&p[2]&&(f=p[2],v=c.cookie.match(new RegExp("(^| )vl-"+f+"=([^;]+)")));var d=c.cookie.match(/(^| )vl-cep=([^;]+)/),k=location[e];if("savedCep"===z&&d&&(!f||"undefined"===typeof f)&&0>k.indexOf("cep=")){var g=-1<k.indexOf("?")?"&":"?";k+=g+d.pop()}a=c.createElement("script");n=c.scripts[0];a.defer=1;a.src=u+(-1===u.indexOf("?")?"?":"&")+"lpref="+l(c.referrer)+
-					"&lpurl="+l(k)+"&lpt="+l(c.title)+"&vtm="+(new Date).getTime()+(v?"&uw=no":"");a[y]=function(){for(m=0;a=c[r][m++];)/dtpCallback\.l/.test(a[e])&&(a[e]=decodeURIComponent(a[e]).match(/dtpCallback\.l="([^"]+)/)[1]);x()};n.parentNode.insertBefore(a,n);f&&(h=new Date,h.setTime(h.getTime()+864E5),c.cookie="vl-"+f+"=1; "+w+"samesite=Strict; expires="+h.toGMTString()+"; path=/")},0),setTimeout(x,7E3))})(window,document,encodeURIComponent,"onreadystatechange","links","href","className","onerror","dtpCallback",
-					0,"https://track.1234sd123.com/d/.js","savedCep");
-		</script>
-		<noscript><link href="https://track.1234sd123.com/d/.js?noscript=true&lpurl=" rel="stylesheet"/></noscript>
-
+        @include('partials.script')
     </head>
-
-<body>
-
-  <div class="blured"> </div>
-
-  <div class="main-text hispan" id="hispan">Are you at least 18 years old? 😊</div>
-
-  <div class="main">
-
-    <div class="stepbox">
-
-      <div class="step step-1 current" id="step-1">
-
-        <div class="step__inner">
-
-          <div class="step__header">
-
-          </div>
-
-          <div class="step__body">
-
-          </div>
-
-          <div class="step__footer">
-
-            <div class="btnbox">
-
-              <a class="btn btn--primary next btn-yes effect effect-5" href="javascript:void(0);">Continue</a>
-
+    <body>
+        <div class="blured"></div>
+        <div class="main-text hispan" id="hispan">{{translate('lp3.are_you_18yo')}} 😊</div>
+        <div class="main">
+            <div class="stepbox">
+                <div class="step step-1 current" id="step-1">
+                    <div class="step__inner">
+                        <div class="step__header"></div>
+                        <div class="step__body"> </div>
+                        <div class="step__footer">
+                            <div class="btnbox">
+                                <a class="btn btn--primary next btn-yes effect effect-5" href="javascript:void(0);">{{translate('lp3.continue')}}</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="step step-7" id="step-2">
+                    <div class="step__inner">
+                        <div class="step__header">
+                            <h2 class="subtitle">{{translate('lp3.you_must_follow_rules')}}</h2>
+                        </div>
+                        <div class="step__body">
+                            <ul>
+                                <li class="item-1">{{translate('lp3.if_you_see_someone')}}</li>
+                                <li class="item-2">{{translate('lp3.when_having_sex')}}
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="step__footer">
+                            <div class="btnbox">
+                                <a href="{{$_ENV['REDIRECT_URL']}}" class="btn btn--primary btn-continue btn-fin go">{{translate('lp3.continue')}}</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-
-          </div>
-
         </div>
-
-      </div>
-
-      <div class="step step-7" id="step-2">
-
-        <div class="step__inner">
-
-          <div class="step__header">
-
-            <h2 class="subtitle">You must follow the RULES given below</h2>
-
-          </div>
-
-          <div class="step__body">
-
-            <ul>
-
-              <li class="item-1">If you see someone you know, DO NOT publicize it. Do not spread rumors.</li>
-
-              <li class="item-2">When having sex with one of our members, it is your responsibility to protect yourself against STDs.</li>
-
-            </ul>
-
-          </div>
-
-          <div class="step__footer">
-
-            <div class="btnbox">
-
-              <a href="{{$_ENV['REDIRECT_URL']}}" class="btn btn--primary btn-continue btn-fin go">Continue</a>
-
-            </div>
-
-          </div>
-
+        <!-- main-->
+        <div class="bg">
+            <div class="bg__item bg__item-1 active" id="1"></div>
+            <div class="bg__item bg__item-7" id="2"></div>
         </div>
-
-      </div>
-
-    </div>
-
-  </div>
-
-  <!-- main-->
-
-  <div class="bg">
-
-    <div class="bg__item bg__item-1 active" id="1">
-
-    </div>
-
-    <div class="bg__item bg__item-7" id="2">
-
-    </div>
-
-  </div>
-
-  <script src="{{asset("/js/app.js")}}"></script>
-
-</body>
-
+        <script src="{{asset("/js/app.js")}}"></script>
+    </body>
 </html>
