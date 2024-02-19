588aec3abc3c560ea3339cd37f095131fe73fb54
Vladimir <vrakita@yahoo.com>
2024-02-19 12:12:34 +0100

    Added lp15.

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -5,7 +5,7 @@ namespace App\Controllers;
 class HomeController extends BaseController {
 
     const AVAILABLE_TEMPLATES = [
-        'lp1', 'lp2', 'lp3', 'lp4', 'lp5', 'lp6', 'lp7', 'lp8', 'lp9', 'lp10', 'lp11', 'lp12', 'lp13', 'lp14'
+        'lp1', 'lp2', 'lp3', 'lp4', 'lp5', 'lp6', 'lp7', 'lp8', 'lp9', 'lp10', 'lp11', 'lp12', 'lp13', 'lp14', 'lp15'
     ];
 
     public function index($template)
